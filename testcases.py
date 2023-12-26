c42740cbd497391442d4e278be93c9f076906b72
Shayan Ali <shayan.ali.work@gmail.com>
2023-12-26 13:25:59 -0500

    added test cases

---
+++ testcases.py
@@ -0,0 +1,47 @@
+test_cases = [
+    {"input": "for the actor - Payer using digital banking services and JTBD- to pay for merchant orders Q. for the above statement, what grade will these outcomes get? Desired outcome Reduce Payment Processing time to few seconds Actual outcome Average Payment Processing time high Payment processing time is the total time required to get reflected in receiver’s account",
+    
+    "expected_output": '2/3. The desired and actual outcomes are related to the end of the Job To Be Done and the measurement is clearly defined. However, there is no numerical value associated with the desired and actual outcome.'
+    },
+    {
+    "input": "evaluate the job to be done 'onboard passengers' for the actor 'pilot'",
+     "expected_output": 
+     "Score: 3/4. The Job To Be Done (JTBD) 'onboard passengers' for the actor 'pilot' is functional and singular, as it represents a single task that the pilot might need to complete. It is also accurate as it does not include any unnecessary details or adjectives. However, it may not be entirely appropriate. While a pilot does play a role in the process of onboarding passengers, this task is typically handled by flight attendants or ground staff. The pilot's primary job is to fly the plane. Therefore, the JTBD could be more appropriately defined for the actor 'pilot'."
+    },
+    {
+        "input":"Help me grade this students outcome: DEsired outcome: average trip time, Actual outcome decreased trip time",
+        "expected_output":"Score: 2/3. The student's outcome statement includes both the actual and desired outcomes, and a measurement metric (trip time). However, it lacks a numerical value associated with the desired and actual outcome. The student should include a specific numerical value to fully meet the grading criteria."
+    },
+        {
+        "input":    ,
+        "expected_output":
+    },
+        {
+        "input":
+        "expected_output":
+    },
+        {
+        "input":
+        "expected_output":
+    },
+        {
+        "input":
+        "expected_output":
+    },
+        {
+        "input":
+        "expected_output":
+    },
+        {
+        "input":
+        "expected_output":
+    },
+        {
+        "input":
+        "expected_output":
+    },
+        {
+        "input":
+        "expected_output":
+    },
+]
